53949b7e5257e02a0b0328bf541fa58bbeb7d294
LiuDeHao <2535771510@qq.com>
2026-09-01 15:39:04 +0800

    chore: remove misplaced agent demo

--- agent_demo/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Tools used by the Fault Diagnosis Agent."""
-
-from .frt_diagnosis import FRTDiagnosisTool
-from .signal_analysis import SignalAnalysisTool
-from .knowledge_retrieval import KnowledgeRetrievalTool
-
-__all__ = ["FRTDiagnosisTool", "SignalAnalysisTool", "KnowledgeRetrievalTool"]
